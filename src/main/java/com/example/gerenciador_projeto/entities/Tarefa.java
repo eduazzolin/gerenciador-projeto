e4e242b2cbb628c7ee01f98f82d2e8be49169b3d
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-11-18 20:57:06 -0300

    refac/descricao tarefa

--- src/main/java/com/example/gerenciador_projeto/entities/Tarefa.java
+++ src/main/java/com/example/gerenciador_projeto/entities/Tarefa.java
@@ -19,6 +19,8 @@ public class Tarefa {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private String nome;
+
+   @Column(length = 1000)
    private String descricao;
    private LocalDateTime dataCriacao;
    private boolean isDeleted;
