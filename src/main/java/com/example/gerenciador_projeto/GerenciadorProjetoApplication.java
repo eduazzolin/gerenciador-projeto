33bdc7e6f807e405b9437f7a4ecca261ebfe49fd
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-11-20 12:02:19 -0300

    test/validacoes

--- src/main/java/com/example/gerenciador_projeto/GerenciadorProjetoApplication.java
+++ src/main/java/com/example/gerenciador_projeto/GerenciadorProjetoApplication.java
@@ -1,6 +1,5 @@
 package com.example.gerenciador_projeto;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GerenciadorProjetoApplication {
 
    public static void main(String[] args) {
-      Dotenv dotenv = Dotenv.load();
-      dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
       SpringApplication.run(GerenciadorProjetoApplication.class, args);
    }
 
--- src/test/java/com/example/gerenciador_projeto/util/TestValidacoes.java
+++ src/test/java/com/example/gerenciador_projeto/util/TestValidacoes.java
@@ -3,6 +3,7 @@ package com.example.gerenciador_projeto.util;
 import com.example.gerenciador_projeto.dto.ProjetoDTO;
 import com.example.gerenciador_projeto.dto.TarefaDTO;
 import com.example.gerenciador_projeto.entities.Status;
+import com.example.gerenciador_projeto.entities.Usuario;
 import com.example.gerenciador_projeto.service.StatusService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -81,5 +81,65 @@ public class TestValidacoes {
       dto.setIdUsuario(1L);
       dto.setDataCriacao(LocalDateTime.now());
       assertTrue(validacoes.validarProjetoDTO(dto));
+   }
+
+   @Test
+   public void testInvalidProjetoDTO() {
+      ProjetoDTO dto = new ProjetoDTO();
+      dto.setNome("P");
+      dto.setDescricao("D");
+      dto.setIdUsuario(1L);
+      dto.setDataCriacao(LocalDateTime.now());
+      assertFalse(validacoes.validarProjetoDTO(dto));
+   }
+
+   @Test
+   public void testValidTarefaDTO() {
+      TarefaDTO dto = new TarefaDTO();
+      dto.setNome("Tarefa Válida");
+      dto.setDescricao("Descrição Válida");
+      dto.setIdProjeto(1L);
+      dto.setIdStatus(1L);
+      dto.setDataCriacao(LocalDateTime.now());
+      assertTrue(validacoes.validarTarefaDTO(dto));
+   }
+
+   @Test
+   public void testInvalidTarefaDTO() {
+      TarefaDTO dto = new TarefaDTO();
+      dto.setNome("T");
+      dto.setDescricao("D");
+      dto.setIdProjeto(1L);
+      dto.setIdStatus(1L);
+      dto.setDataCriacao(LocalDateTime.now());
+      assertFalse(validacoes.validarTarefaDTO(dto));
+   }
+
+   @Test
+   public void testInvalidEmail() {
+      assertFalse(validacoes.validarEmail("email"));
+   }
+
+   @Test
+   public void testValidEmail() {
+      assertTrue(validacoes.validarEmail("email@email.com"));
+   }
+
+   @Test
+   public void testValidUsuario() {
+      Usuario usuario = new Usuario();
+      usuario.setNome("Nome Válido");
+      usuario.setEmail("email@email.com");
+      usuario.setSenha("senha");
+      assertTrue(validacoes.validarUsuario(usuario));
+   }
+
+   @Test
+   public void testInvalidUsuario() {
+      Usuario usuario = new Usuario();
+      usuario.setNome("N");
+      usuario.setEmail("email");
+      usuario.setSenha("");
+      assertFalse(validacoes.validarUsuario(usuario));
    }
 }
